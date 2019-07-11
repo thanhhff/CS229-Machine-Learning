7252e0502f94ef887017ced19773a37f6966c05f
Thanhhff <thanhnguyen200399@gmail.com>
2019-07-11 17:40:30 +0700

    ex1_multi

--- machine-learning-ex1/ex1/computeCostMulti.m
+++ machine-learning-ex1/ex1/computeCostMulti.m
@@ -13,8 +13,8 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
-
+J = (1/(2*m)) * sum( (X*theta-y).^2 );
+% J = (1/ 2*m) * sum ((X*theta -y).^2); <- Fail: ( ) 
 
 % =========================================================================
 
